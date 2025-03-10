d446ed10bcd0c01cf8dea8cb2ab915ef3091e9be
shafeeq777444 <kp.shafeeqmohamed@gmail.com>
2025-03-10 21:53:01 +0530

    sixth

--- app/(root)/meeting/[id]/page.tsx
+++ app/(root)/meeting/[id]/page.tsx
@@ -13,7 +13,8 @@ import MeetingRoom from '@/components/MeetingRoom';
 import '@stream-io/video-react-sdk/dist/css/styles.css';
 
 const MeetingPage = () => {
-  const { id } = useParams();
+  const params = useParams();
+  const id = params?.id as string | undefined;
   const { isLoaded, user } = useUser();
   const { call, isCallLoading } = useGetCallById(id ?? "");
   const [isSetupComplete, setIsSetupComplete] = useState(false);
@@ -24,7 +25,7 @@ const MeetingPage = () => {
     <p className="text-center text-3xl font-bold text-white">
       Call Not Found
     </p>
-  );
+  );   
 
   // get more info about custom call type:  https://getstream.io/video/docs/react/guides/configuring-call-types/
   const notAllowed = call.type === 'invited' && (!user || !call.state.members.find((m) => m.user.id === user.id));
