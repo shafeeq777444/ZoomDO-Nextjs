ca6a825d8790df403c771155e6019115732b45fd
shafeeq777444 <kp.shafeeqmohamed@gmail.com>
2025-03-10 21:38:11 +0530

    four

--- components/MeetingCard.tsx
+++ components/MeetingCard.tsx
@@ -1,77 +1,64 @@
 "use client";
 
 import Image from "next/image";
-
 import { cn } from "@/lib/utils";
 import { Button } from "./ui/button";
 import { toast } from "sonner";
 
 interface MeetingCardProps {
-  title: string;
-  date: string;
-  icon: string;
-  isPreviousMeeting?: boolean;
-  buttonIcon1?: string;
-  buttonText?: string;
-  handleClick: () => void;
-  link: string;
+    title: string;
+    date: string;
+    icon: string;
+    isPreviousMeeting?: boolean;
+    buttonIcon1?: string;
+    buttonText?: string;
+    handleClick: () => void;
+    link: string;
 }
 
 const MeetingCard = ({
-  icon,
-  title,
-  date,
-  isPreviousMeeting,
-  buttonIcon1,
-  handleClick,
-  link,
-  buttonText,
+    icon,
+    title,
+    date,
+    isPreviousMeeting,
+    buttonIcon1,
+    handleClick,
+    link,
+    buttonText,
 }: MeetingCardProps) => {
-  
-
-  return (
-    <section className="flex min-h-[258px] w-full flex-col justify-between rounded-[14px] bg-[#393760] px-5 py-8 xl:max-w-[568px]">
-      <article className="flex flex-col gap-5">
-        <Image src={icon} alt="upcoming" width={28} height={28} />
-        <div className="flex justify-between">
-          <div className="flex flex-col gap-2">
-            <h1 className="text-2xl font-bold">{title}</h1>
-            <p className="text-base font-normal">{date}</p>
-          </div>
-        </div>
-      </article>
-      <article className={cn("flex justify-center relative", {})}>
-        
-        {!isPreviousMeeting && (
-          <div className="flex gap-2">
-            <Button onClick={handleClick} className="rounded bg-[#2E2C54] px-6">
-              {buttonIcon1 && (
-                <Image src={buttonIcon1} alt="feature" width={20} height={20} />
-              )}
-              &nbsp; {buttonText}
-            </Button>
-            <Button
-              onClick={() => {
-                navigator.clipboard.writeText(link);
-                toast(
-                   "Link Copied",
-                );
-              }}
-              className="bg-[#2E2C54] px-6"
-            >
-              <Image
-                src="/icons/copy.svg"
-                alt="feature"
-                width={20}
-                height={20}
-              />
-              &nbsp; Copy Link
-            </Button>
-          </div>
-        )}
-      </article>
-    </section>
-  );
+    return (
+        <section className="flex min-h-[258px] w-full flex-col justify-between rounded-[14px] bg-[#393760] px-5 py-8 xl:max-w-[568px]">
+            <article className="flex flex-col gap-5">
+                <Image src={icon} alt="upcoming" width={28} height={28} />
+                <div className="flex justify-between">
+                    <div className="flex flex-col gap-2">
+                        <h1 className="text-2xl font-bold">{title}</h1>
+                        <p className="text-base font-normal">{date}</p>
+                    </div>
+                </div>
+            </article>
+            <article className={cn("flex justify-center relative", {})}>
+                {!isPreviousMeeting && (
+                    <div className="flex gap-2">
+                        <Button onClick={handleClick} className="rounded bg-[#2E2C54] px-6">
+                            {buttonIcon1 && <Image src={buttonIcon1} alt="feature" width={20} height={20} />}
+                            &nbsp; {buttonText}
+                        </Button>
+                        <Button
+                            onClick={() => {
+                                navigator.clipboard.writeText(link);
+                                toast("Link Copied");
+                            }}
+                            className="bg-[#2E2C54] px-6"
+                        >
+                            <Image src="/icons/copy.svg" alt="feature" width={20} height={20} />
+                            &nbsp; Copy Link
+                        </Button>
+                    </div>
+                )}
+            </article>
+        </section>
+    );
 };
 
 export default MeetingCard;
